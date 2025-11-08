eb3fa9ceb3a98410678c5d44f92df4d37757755f
scaloor <admin@scaloor.com>
2025-11-08 12:10:06 +1100

    fixing build error, updating auth config environment variable

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -3,7 +3,7 @@ const config = {
     {
       // Configure CLERK_JWT_ISSUER_DOMAIN on the Convex Dashboard
       // See https://docs.convex.dev/auth/clerk#configuring-dev-and-prod-instances
-      domain: process.env.CLERK_JWT_ISSUER_DOMAIN,
+      domain: process.env.CLERK_FRONTEND_API_URL,
       applicationID: "convex",
     },
   ],
